215b9cf5ca8562f0e0378fedc69173c34efe8e68
sulemankhalid128 <sulemankhalid128@gmail.com>
2024-07-20 20:22:37 +0500

    implement the supabase update with prisma

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -9,24 +9,25 @@ export async function POST(req: NextRequest) {
     const data = await req.json();
     console.log('-------> data', data)
     const { email, password, confirmPassword,age, ...result } = data;
-    const existingUser = await prisma.user.findUnique({
+    const existingUser:any = await prisma.user.findUnique({
       where: { email },
     });
 
     if(!existingUser){
-    //   let user = await signUp(email,password)
-    //   if(user){
-    // const newUser = await prisma.user.create({
-    //   data: {
-    //     supabaseId: "940ffbd9-ad5a-4d24-b0e8-b03c0888cdd7", // Replace with actual supabase ID if necessary
-    //     ...result,
-    //     email,
-    //     age: parseInt(age)
-    //   },
-    // });
+      let user = await signUp(email,password)
+      if(user){
+    const newUser = await prisma.user.create({
+      data: {
+        supabaseId: "940ffbd9-ad5a-4d24-b0e8-b03c0888cdd7", // Replace with actual supabase ID if necessary
+        ...result,
+        email,
+        age: parseInt(age)
+      },
+    });
     return NextResponse.json({user:'user'}, { status: 201 });
   }
-  return NextResponse.json({error:`Email ${existingUser.email} already exist!`},{status:402})
+}
+  return NextResponse.json({error:`Email ${existingUser?.email} already exist!`},{status:402})
 
   
   } catch (error:any) {
